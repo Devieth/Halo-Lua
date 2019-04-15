e79755960c8f1c5bcfefac746199e4ca87c750b0
Devieth <jb5685841@gmail.com>
2019-04-15 06:34:59 -0500

    Add files via upload

---
+++ antispeedhack.lua
@@ -0,0 +1,44 @@
+-- Anti-speedhack by Devieth
+-- For SAPP
+
+mode = "k" -- k, b or ipban
+
+api_version = "1.10.0.0"
+update_violation_count = {}
+past_update = {}
+
+function OnScriptLoad()
+	register_callback(cb['EVENT_TICK'], "OnEventTick")
+	timer(5000, "check_violations")
+end
+
+function OnScriptUnload() end
+
+function OnEventTick()
+	for i = 1,16 do
+		if player_present(i) then
+			local m_player = get_player(i)
+			local update = read_word(m_player + 0xF4)
+			if past_update[i] then
+				local difference = update - past_update[i]
+				if difference > 1 and difference < 4 then
+					if update_violation_count[i] then
+						update_violation_count[i] = update_violation_count[i] + 1
+						if update_violation_count[i] >= 5 then
+							execute_command(mode.." "..i.. " Speedhack")
+						end
+					else
+						update_violation_count[i] = 1
+					end
+				end
+			end
+			past_update[i] = update
+		end
+	end
+end
+
+function check_violations()
+	update_violation_count = {}
+	return true
+end
+
