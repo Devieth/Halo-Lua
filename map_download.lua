8d21fa8a152b8df6789c79edf2db7c7f2863bd14
Devieth <jb5685841@gmail.com>
2019-04-17 13:25:07 -0500

    Add files via upload

--- map_download.lua
+++ map_download.lua
@@ -6,33 +6,38 @@
 -- 7z.exe
 -- 7z.dll
 
-map_repo = "http://maps.halonet.net/maps/" -- Hac2 Repo
+hac2_repo = "http://maps.halonet.net/" -- HAC2 Repo
+hac2_legacy_repo = "http://maps.haloanticheat.com/" -- Legacy HAC2 Repo
+
 api_version = "1.10.0.0"
 
 function OnScriptLoad()
 	register_callback(cb['EVENT_COMMAND'], "OnEventCommand")
-	maps_folder_path = read_string(0x5B9610).."\\maps\\"
+	-- Assign the repo to use.
+	map_repo = hac2_repo
+	-- Get the EXE Path
+	exe_path = read_string(read_dword(sig_scan("0000BE??????005657C605") + 0x3))
+	-- Split the string
+	local t = tokenizestring(exe_path, "\\")
+	-- Create the maps_folder_path
+	maps_folder_path = string.sub(exe_path, 0, string.len(exe_path) - string.len(t[#t])).."\\maps\\"
 end
 
 function OnScriptUnload() end
 
--- Enviroment
--- 0 = Console
--- 1 = RCON Console
--- 2 = Chat
 function OnEventCommand(PlayerIndex, Command, Enviroment, Password)
-	local t = tokenizestring(string.gsub(Command, [["]], ""), " ")
+	local t = tokenizestring(string.lower(string.gsub(Command, '"', "")), " ")
 	if get_var(PlayerIndex, "$lvl") ~= "-1" or tonumber(Enviroment) == 0 then
 
 		-- Hi-Jack SAPP's command.
 		if t[1] == "map_download" then
 
-			-- Make sure they actuall used a name
+			-- Make sure they actually used a name
 			if t[2] then
 				local downloaded, map_name = false, tostring(t[2])
 
 				-- Download the map.
-				local map = assert(io.popen('wget -c '..map_repo..map_name..'.zip'))
+				local map = assert(io.popen('wget -c '..map_repo.."map_download.php?map="..map_name))
 				map:close()
 
 				-- Check if the download was successful.
@@ -44,7 +49,7 @@ function OnEventCommand(PlayerIndex, Command, Enviroment, Password)
 
 				if downloaded then
 					-- Unzip the map.
-					local sevenz = assert(io.popen('7z.exe e '..tostring(t[2])..'.zip -o'..maps_folder_path))
+					local sevenz = assert(io.popen('7z.exe e '..map_name..'.zip -o'..maps_folder_path))
 					sevenz:close()
 
 					-- Load the map.
@@ -57,12 +62,26 @@ function OnEventCommand(PlayerIndex, Command, Enviroment, Password)
 
 					-- Alert the usere that the download failed.
 					rcon_return(tonumber(Enviroment), PlayerIndex, "Failed to download "..map_name)
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Map not on repo or was misspelled.")
+					rcon_return(tonumber(Enviroment), PlayerIndex, "Check the spelling of the map.")
+					rcon_return(tonumber(Enviroment), PlayerIndex, "Check the repo for the map @ http://maps.halonet.net/maplist.php")
 				end
 				return false
 			else
+
+				-- Let them know how to use the hi-jacked command.
 				rcon_return(tonumber(Enviroment), PlayerIndex, "map_download <map name>")
 				return false
+			end
+		elseif t[1] == "repo" then
+			if t[2] then
+				if t[2] == "legacy" then
+					map_repo = hac2_legacy_repo
+				else
+					map_repo = tostring(t[2])
+				end
+				rcon_return(tonumber(Enviroment), PlayerIndex, "Map Repo set to: "..repo)
+			else
+				rcon_return(tonumber(Enviroment), PlayerIndex, "Current Map Repo: "..repo)
 			end
 		end
 	end
