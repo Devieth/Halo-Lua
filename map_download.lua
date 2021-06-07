3313359cabd0f205f8c598b64b5fe37af366f010
Devieth <5672855+Devieth@users.noreply.github.com>
2021-06-06 21:59:00 -0500

    Update map_download.lua

--- map_download.lua
+++ map_download.lua
@@ -1,149 +1,46 @@
--- Map Downloader by Devieth
--- For SAPP
+-- Map Downloader 2.0 by Devieth
+-- For SAPP 10.x.x
 
--- Required files:
--- wget.exe
--- 7z.exe
--- 7z.dll
-
-hac2_repo = "http://maps.halonet.net/" -- HAC2 Repo
-hac2_legacy_repo = "http://maps.haloanticheat.com/" -- Legacy HAC2 Repo
-
-api_version = "1.10.0.0"
+-- HAC2 repo URL
+hac2_repo = 'http://maps.halonet.net/' -- HAC2 Repo
+-- If you have another source for these files feel free to use it.  `halopc.com` may not be up forever.
+wget_dl = 'http://halopc.com/download/wget.exe'
+unzip_dl = 'http://halopc.com/download/unzip.exe'
+-- Sapp API min version.
+api_version = '1.10.0.0'
+script = 'map_download.lua: \t'
 
 function OnScriptLoad()
-	register_callback(cb['EVENT_COMMAND'], "OnEventCommand")
+	register_callback(cb['EVENT_COMMAND'], 'OnEventCommand')
 	-- Assign the repo to use.
 	map_repo = hac2_repo
 	-- Get the EXE Path
-	exe_path = read_string(read_dword(sig_scan("0000BE??????005657C605") + 0x3))
+	exe_path = read_string(read_dword(sig_scan('0000BE??????005657C605') + 0x3))
 	-- Split the string
-	local t = tokenizestring(exe_path, "\\")
+	local t = tokenizestring(exe_path, '\\')
 	-- Create the maps_folder_path
-	maps_folder_path = string.sub(exe_path, 0, string.len(exe_path) - string.len(t[#t])).."\\maps\\"
+	exe_folder_path = string.sub(exe_path, 0, string.len(exe_path) - string.len(t[#t]))
+	maps_folder_path = string.sub(exe_path, 0, string.len(exe_path) - string.len(t[#t]))..'maps\\'
+	-- Download unzip.
+	get_required_files()
 end
 
 function OnScriptUnload() end
 
-function OnEventCommand(PlayerIndex, Command, Enviroment, Password)
-	local t = tokenizestring(string.lower(string.gsub(Command, '"', "")), " ")
-	if get_var(PlayerIndex, "$lvl") ~= "-1" or tonumber(Enviroment) == 0 then
-
-		-- Hi-Jack SAPP's command.
-		if t[1] == "map_download" then
-
-			-- Make sure they actually used a name
-			if t[2] then
-				local downloaded, map_name = false, tostring(t[2])
-
-				-- Download the map.
-				local map = assert(io.popen('wget -O "'..map_name..'" -c '..map_repo.."map_download.php?map="..map_name))
-				map:close()
-
-				-- Check if the download was successful.
-				local file = io.open(map_name)
-				if file then
-					downloaded = true
-					file:close()
-				end
-
-				if downloaded then
-					-- Unzip the map.
-					local sevenz = assert(io.popen('7z.exe e "'..map_name..'" -o'..maps_folder_path))
-					sevenz:close()
-
-					-- Load the map.
-					execute_command("map_load "..map_name)
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Download of "..map_name.. " complete!")
-
-					-- Delete the .zip file.
-					os.remove(map_name)
-				else
-
-					-- Alert the usere that the download failed.
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Failed to download "..map_name)
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Check the spelling of the map.")
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Check the repo for the map @ http://maps.halonet.net/maplist.php")
-				end
-			else
-				-- Let them know how to use the hi-jacked command.
-				rcon_return(tonumber(Enviroment), PlayerIndex, "map_download <map name>")
-			end
-			return false
-		elseif t[1] == "map_download.map" then
-			if t[2] then
-				if t[3] then
-					local downloaded, URL, map_name = false, tostring(t[2]), tostring(t[3])
-
-					-- Download the map.
-					local map = assert(io.popen('wget -c '..URL..' -O '..maps_folder_path..map_name))
-					map:close()
-
-					--Load the map
-					execute_command("map_load "..map_name)
-				else
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Please include the full map and file name. EX: cmt_snow_grove.map")
-				end
-			else
-				rcon_return(tonumber(Enviroment), PlayerIndex, "map_download.map <URL> <map_name.map>")
-			end
-			return false
-		elseif t[1] == "map_download.zip" then
-			if t[2] then
-				if t[3] then
-					local downloaded, URL, map_name = false, tostring(t[2]), tostring(t[3])
-
-					-- Download the map.
-					local map = assert(io.popen('wget -O "'..map_name..'" -c '..URL))
-					map:close()
-
-					-- Check if the download was successful.
-					local file = io.open(map_name)
-					if file then
-						downloaded = true
-						file:close()
-					end
-
-					if downloaded then
-						-- Unzip the map.
-						local sevenz = assert(io.popen('7z.exe e "'..map_name..'" -o'..maps_folder_path))
-						sevenz:close()
-						-- Load the map.
-						execute_command("map_load "..map_name)
-						rcon_return(tonumber(Enviroment), PlayerIndex, "Download of "..map_name.." complete!")
-						-- Delete the .zip file.
-						os.remove(map_name)
-					else
-						-- Alert the usere that the download failed.
-						rcon_return(tonumber(Enviroment), PlayerIndex, "Download failed.")
-					end
-				else
-					rcon_return(tonumber(Enviroment), PlayerIndex, "Map name required! Ex: garden_ce")
-				end
-			else
-				rcon_return(tonumber(Enviroment), PlayerIndex, "map_download.zip <URL> <map_name>")
-			end
-			return false
-		elseif t[1] == "repo" then
-			if t[2] then
-				if t[2] == "legacy" then
-					map_repo = hac2_legacy_repo
-				else
-					map_repo = tostring(t[2])
-				end
-				rcon_return(tonumber(Enviroment), PlayerIndex, "Map Repo set to: "..repo)
-			else
-				rcon_return(tonumber(Enviroment), PlayerIndex, "Current Map Repo: "..repo)
-			end
-			return false
-		end
+function tokenizestring(input_string, delimiter)
+	if delimiter == nil then delimiter = '%s' end
+	local t = {}
+	for str in string.gmatch(input_string, '([^'..delimiter..']+)') do
+		t[#t+1] = str
 	end
+	return t
 end
 
-function rcon_return(Enviroment, PlayerIndex, Message)
-	local Compatable_Message = string.gsub(tostring(Message), "|t", "	")
+function rcon_return(Enviroment, PlayerIndex, Message, Color)
+	local Compatable_Message = string.gsub(tostring(Message), '|t', '	')
+	if Color == nil then Color = 14 end
 	if Enviroment == 0 then
-		cprint(Compatable_Message,14)
+		cprint(Compatable_Message, Color)
 	elseif Enviroment == 1 then
 		rprint(PlayerIndex, Message)
 	elseif Enviroment == 2 then
@@ -151,14 +48,88 @@ function rcon_return(Enviroment, PlayerIndex, Message)
 	end
 end
 
-function tokenizestring(inputstr, sep)
-	if sep == nil then
-		sep = "%s"
+function download_file(url, path, filename, powershell)
+	if filename == nil then filename = '' end
+	if powershell then
+		os.execute("powershell (New-Object System.Net.WebClient).DownloadFile('"..url.."','"..path..filename.."')")
+	else
+		local file = assert(io.popen('wget -O "'..filename..'" -c '..url))
+		file:close()
+		local file = io.open(path..filename)
+		if file then
+			local content_size = string.len(file:read('*all'))
+			file:close()
+			if content_size > 0 then
+				return true
+			else
+				os.remove(filename)
+				return false
+			end
+		end
+		return false
 	end
-	local t={} ; i=1
-	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-		t[i] = str
-		i = i + 1
+	cprint(script.."Download complete! Saving file to: "..path..filename, 14)
+end
+
+function get_required_files()
+	local file = io.open('unzip.exe', 'r')
+	if not file then
+		cprint(script..'unzip.exe not present, attempting to download...', 4)
+		download_file(unzip_dl, exe_folder_path, 'unzip.exe', true)
+	else
+		cprint(script..'unzip.exe found!',14)
+		file:close()
 	end
-	return t
+	local file = io.open('wget.exe', 'r')
+	if not file then
+		cprint(script..'wget.exe not present, attempting to download...', 4)
+		download_file(wget_dl, exe_folder_path, 'wget.exe', true)
+	else
+		cprint(script..'wget.exe found!',14)
+		file:close()
+	end
+end
+
+function OnEventCommand(PlayerIndex, Command, Enviroment, Password)
+	local t = tokenizestring(string.lower(string.gsub(Command, '"', '')), ' ')
+	if get_var(PlayerIndex, '$lvl') ~= '-1' or tonumber(Enviroment) == 0 then
+		-- Hi-Jack SAPP's command.
+		if t[1] == 'map_download' then
+			-- Make sure they actually used a name
+			if t[2] then
+				-- Download the map
+				local map_name = t[2]
+				-- Check if they already have the map, or wget is going to hold the server hostage.
+				local file = io.open(maps_folder_path..map_name..'.map')
+				if  file then
+					file:close()
+					rcon_return(tonumber(Enviroment), PlayerIndex, script..map_name.. " is already installed!")
+					return false
+				end
+				-- Download the map.
+				rcon_return(tonumber(Enviroment), PlayerIndex, script.."WARNING: Server may lag or disconnect durring download!")
+				local downloaded = download_file(hac2_repo..'map_download.php?map='..map_name, exe_folder_path, map_name..'.zip')
+				if downloaded then
+					local unzip = assert(io.popen('unzip '..map_name..' -d '..maps_folder_path))
+					unzip:close()
+					-- Check to make sure the file was unziped.
+					local file = io.open(maps_folder_path..map_name..'.map')
+					if file then
+						file:close()
+						execute_command("map_load "..map_name)
+						rcon_return(tonumber(Enviroment), PlayerIndex, script.."Download of "..map_name.. " complete!")
+						-- Delete the .zip file.
+						os.remove(map_name..'.zip')
+					else
+						rcon_return(tonumber(Enviroment), PlayerIndex, script..'Error: Failed to extract '..map_name..'.zip', 4)
+					end
+				else
+					rcon_return(tonumber(Enviroment), PlayerIndex, script..'Error: Failed to download '..map_name..'\n'..script..'Please check your spelling or the hac2 repo if the map exist.', 4)
+				end
+			else
+				rcon_return(tonumber(Enviroment), PlayerIndex, script..'Error: Please enter a map name.', 4)
+			end
+			return false
+		end
+	end
 end
